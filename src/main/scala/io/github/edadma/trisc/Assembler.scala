b3a2bb9d020ecdb1bf3a7b671d8b0067cb2c0427
Edward A Maxedon Sr <edadma@gmail.com>
2023-05-28 12:47:03 -0400

    assembler

--- src/main/scala/io/github/edadma/trisc/Assembler.scala
+++ src/main/scala/io/github/edadma/trisc/Assembler.scala
@@ -1,6 +1,37 @@
 package io.github.edadma.trisc
 
-import pprint.pprintln
+import scala.collection.mutable
 
-object Assembler:
-  def apply(code: String):
+trait Chunk
+case class DataChunk(data: Seq[Byte]) extends Chunk
+
+case class Segment(org: Long, chunks: Seq[Chunk])
+
+class Assembler(stacked: Boolean = false):
+  private class Pass1Segment:
+    var size: Long = 0
+    val symbols = new mutable.HashMap[String, Long]
+
+  def assemble(code: String): Unit /*Seq[Segment]*/ =
+    val lines = AssemblyParser.parseAssembly(code)
+    val equates = new mutable.HashMap[String, ExprAST]
+    val segments = new mutable.LinkedHashMap[String, Pass1Segment]
+    var segment = new Pass1Segment
+
+    segments("_default_") = segment
+
+    lines foreach {
+      case SegmentLineAST(name) =>
+        segments get name match
+          case None =>
+            segment = new Pass1Segment
+            segments(name) = segment
+          case Some(s) => segment = s
+      case label @ LabelLineAST(name) =>
+        if segments.exists((_, s) => s.symbols contains name) then problem(label.pos, s"duplicate label '$name'")
+        if equates contains name then problem(label.pos, s"equate '$name' already defined")
+        segment.symbols(name) = segment.size
+      case equate @ EquateLineAST(name, expr) =>
+        if equates contains name then problem(equate.pos, s"duplicate equate '$name'")
+        if segments.exists((_, s) => s.symbols contains name) then problem(equate.pos, s"label '$name' already defined")
+    }
