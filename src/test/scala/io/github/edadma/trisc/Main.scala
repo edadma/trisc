1c9ff9ba692a4185af3bee3da9ef686adc7daf11
Edward A Maxedon Sr <edadma@gmail.com>
2023-05-29 18:19:38 -0400

    assembler

--- src/test/scala/io/github/edadma/trisc/Main.scala
+++ src/test/scala/io/github/edadma/trisc/Main.scala
@@ -13,10 +13,15 @@ import pprint.pprintln
       |reset
       |  ldi r1, 1          // start counter at 1
       |  ldi r3, STDOUT     // r3 contains stdout device address
+      |loop
       |  addi r4, r1, '0'   // convert counter to ASCII character
-      |  stb r3, r0, r4 // output counter character
-      |  sti r3, '\n' // output linefeed character
-      |  beq r0, r0, reset
+      |  stb r3, r0, r4     // output counter character
+      |  sti r3, '\n'       // output linefeed character
+      |  addi  r1, r1, 1    // increment counter
+      |  ldi r2, 5          // we so that we can compare counter to 5
+      |  bls r2, r1, 2      // is 5 < counter? if so, jump to end program
+      |  beq r0, r0, loop   // jump back to loop start for next iteration
+      |  brk                // end program
       |  """.stripMargin,
   )
 
