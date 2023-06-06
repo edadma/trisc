2a00433727592e29d045705818627e13c3d0deef
Edward A Maxedon Sr <edadma@gmail.com>
2023-06-06 17:20:13 -0400

    emulator

--- src/main/scala/io/github/edadma/trisc/Emulator.scala
+++ src/main/scala/io/github/edadma/trisc/Emulator.scala
@@ -1,3 +1,12 @@
 package io.github.edadma.trisc
 
-class Emulator:
+class Emulator(memory: Seq[(String, String, Long)]) extends Addressable:
+  def load(tof: TOF): Unit = {}
+
+  def run(): Unit = {}
+
+  def step(): Unit = {}
+
+  def registers: (Long, Long) = null
+
+  def disassemble: String = null
