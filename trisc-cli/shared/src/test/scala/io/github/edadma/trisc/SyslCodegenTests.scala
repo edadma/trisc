4aaa2e32183d0af9612ba22b8ceda8096b031e72
Edward A Maxedon <edadma@gmail.com>
2026-03-29 12:27:53 -0400

    Add unit tests for negative global val constant codegen
    Tests that val with negative initializers (e.g. val NEG = -1)
    emit correct data via constEval, including comparison, array
    init, and arithmetic with multiple negative constants.

--- trisc-cli/shared/src/test/scala/io/github/edadma/trisc/SyslCodegenTests.scala
+++ trisc-cli/shared/src/test/scala/io/github/edadma/trisc/SyslCodegenTests.scala
@@ -1938,6 +1938,53 @@ class SyslTriscCodegenTests extends AnyFreeSpec with Matchers {
         |""".stripMargin) shouldBe 42
   }
 
+  // ===== Negative constant initializers =====
+
+  "global val with negative value" in {
+    compileAndRun(
+      """val NEG = -1
+        |main() -> int = NEG
+        |""".stripMargin) shouldBe -1
+  }
+
+  "global val with negative value used in comparison" in {
+    compileAndRun(
+      """val EMPTY = -1
+        |var x = -1
+        |
+        |main() -> int
+        |    if x == EMPTY
+        |        42
+        |    else
+        |        0
+        |""".stripMargin) shouldBe 42
+  }
+
+  "global val with negative value in array init" in {
+    compileAndRun(
+      """val NONE = -1
+        |var arr: [4]int
+        |
+        |init()
+        |    var i = 0
+        |    while i < 4
+        |        arr[i] = NONE
+        |        i += 1
+        |
+        |main() -> int
+        |    init()
+        |    arr[0] + arr[1] + arr[2] + arr[3]
+        |""".stripMargin) shouldBe -4
+  }
+
+  "global val with unary minus expression" in {
+    compileAndRun(
+      """val A = -42
+        |val B = -100
+        |main() -> int = A + B
+        |""".stripMargin) shouldBe -142
+  }
+
   "queue enqueue at multiple priorities" in {
     compileAndRun(
       """val EMPTY = -1
